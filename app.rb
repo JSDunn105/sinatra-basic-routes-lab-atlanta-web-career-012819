08dd710665df2a1562906f4063008bf9bd1b5149
@JSDunn105 <j.s.dunn@icloud.com>
2019-02-19 15:24:15 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Ruby. I call a block. I like Sinatra. I don't like Grok. "
+  end
+
+  get '/hometown' do
+
+  "My hometown is Gary Indiana, Gary Indiana, Gary Indiana."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Carolina in My Mind, by James Taylor."
+  end
 end
