242b9c2ee481ca00bdcd346d1dba8b9408c79b1f
@JSDunn105 <j.s.dunn@icloud.com>
2019-04-01 14:34:37 -0400

    New work save

--- app.rb
+++ app.rb
@@ -3,7 +3,6 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-<<<<<<< HEAD
     'Hello World!'
   end
 
@@ -16,25 +15,8 @@ class App < Sinatra::Base
   end
 
   get '/favorite-song' do
-    'My favorite song is "Come Along" by Cosmo Sheldrake.'
+    'My favorite song is "Carolina in My Mind" by JT and my chanson-de-la-semaine is "Come Along" by Cosmo Sheldrake.'
   end
 
-=======
-    "Hello, World!"
-  end
-
-  get '/name' do
-    "My name is Ruby. I call a block. I like Sinatra. I don't like Grok. "
-  end
-
-  get '/hometown' do
-
-  "My hometown is Gary Indiana, Gary Indiana, Gary Indiana."
-  end
-
-  get '/favorite-song' do
-    "My favorite song is Carolina in My Mind, by James Taylor."
-  end
->>>>>>> 08dd710665df2a1562906f4063008bf9bd1b5149
 end
 
