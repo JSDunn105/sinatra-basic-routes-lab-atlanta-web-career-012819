d548638b61bf155a8d5938b2d3216c9d82665150
@JSDunn105 <j.s.dunn@icloud.com>
2019-04-01 12:29:06 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    'Hello World!'
+  end
+
+  get '/name' do
+    'My name is Scotty.'
+  end
+
+  get '/hometown' do
+    'My hometown is Rocky Mount, NC.'
+  end
+
+  get '/favorite-song' do
+    'My favorite song is "Come Along" by Cosmo Sheldrake.'
+  end
+
 end
+
